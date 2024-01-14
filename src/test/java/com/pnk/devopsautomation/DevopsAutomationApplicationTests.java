09242e29925399edfc63dea9256e0e3ceddfba47
Phong Vo <vhphong@gmail.com>
2024-01-14 10:44:49 -0600

    Add existing project files to Git

---
+++ src/test/java/com/pnk/devopsautomation/DevopsAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pnk.devopsautomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
