b7e9aabc4200cb8ebab3f471248c775dc1f8b57e
Phong Vo <vhphong@gmail.com>
2024-01-24 16:38:54 -0600

    @RequestMapping("/devops-automation")

--- src/main/java/com/pnk/devopsautomation/DevopsAutomationApplication.java
+++ src/main/java/com/pnk/devopsautomation/DevopsAutomationApplication.java
@@ -3,6 +3,7 @@ package com.pnk.devopsautomation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DevopsAutomationApplication {
 
     @GetMapping
+    @RequestMapping("/devops-automation")
     public static String message() {
         System.out.println("Hello from Devops Automation Application");
         return "Welcome to Devops Automation Application";
